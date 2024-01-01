2b3aea57a7dbf2c9ecbee3dc085f9a9683dd9497
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2024-01-01 17:49:57 -0500

    Update camera_runner.cpp

--- src/camera_runner.cpp
+++ src/camera_runner.cpp
@@ -205,11 +205,11 @@ void CameraRunner::start() {
                     // from the cameras rotation then remap the data. (for
                     // example from 0 to 90 or 180 to 270)
                     for (int i = 0; i < bound; i++) {
-                    int x = i%(m_height-1);
-                    int y = i/m_width;
-                    //int i = x + y * m_width;
-                    int o = (m_height-1-y) + (x * m_height);
-                    std::memcpy(color_out_buf + o * 3, input_ptr + i * 4, 3);
+                        std::memcpy(color_out_buf +
+                                        ((m_height - 1 - (i / (m_width))) +
+                                         ((i % (m_width)) * m_height)) *
+                                            3,
+                                    input_ptr + i * 4, 3);
                     }
                 } else {
                     for (int i = 0; i < bound; i++) {
@@ -217,16 +217,6 @@ void CameraRunner::start() {
                                     3);
                     }
                 }
-
-                // for (int x = 0; x < m_width; x++) {
-                //     for (int y = 0; y < m_height; y++) {
-
-                //         int i = x + y * m_width;
-                //         int o = (m_height-1-y) + (x * m_height);
-                //         std::memcpy(color_out_buf + o * 3, input_ptr + i * 4,
-                //         3);
-                //     }
-                // }
             }
 
             if (m_copyOutput) {
