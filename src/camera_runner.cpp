a5d516a12acd00fbad7085499ed4ac8f4e923975
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2024-01-01 18:51:28 -0500

    Update camera_runner.cpp

--- src/camera_runner.cpp
+++ src/camera_runner.cpp
@@ -226,9 +226,9 @@ void CameraRunner::start() {
                         int destIndex0 = ((m_height - 1 - (i / m_width)) +
                                           ((i % m_width) * m_height)) *
                                          3;
-                        int destIndex1 = destIndex0 - 3;
-                        int destIndex2 = destIndex0 - 6;
-                        int destIndex3 = destIndex0 - 9;
+                        int destIndex1 = destIndex0 - m_height * 3;
+                        int destIndex2 = destIndex1 - m_height * 3;
+                        int destIndex3 = destIndex2 - m_height * 3;
 
                         std::memcpy(color_out_buf + destIndex0,
                                     input_ptr + srcIndex, 3);
@@ -243,9 +243,9 @@ void CameraRunner::start() {
                     for (int i = 0; i < bound; i += 4) {
                         int srcIndex = i * 4;
                         int destIndex0 = i * 3;
-                        int destIndex1 = destIndex0 + 3;
-                        int destIndex2 = destIndex0 + 6;
-                        int destIndex3 = destIndex0 + 9;
+                        int destIndex1 = destIndex0 + m_height * 3;
+                        int destIndex2 = destIndex1 + m_height * 3;
+                        int destIndex3 = destIndex2 + m_height * 3;
 
                         std::memcpy(color_out_buf + destIndex0,
                                     input_ptr + srcIndex, 3);
